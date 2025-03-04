b4e2c97c82b74e9be6d73367a5b9915431c8fda1
José Otávio Junqueira Ramos <joseotavio.jr1104@gmail.com>
2025-03-04 16:57:41 -0300

    hamburguer button

--- scripts.js
+++ scripts.js
@@ -1,14 +1,4 @@
-const text = "Este é um efeito de digitação!";
-let index = 0;
-const speed = 100; // Velocidade do efeito em milissegundos
-const textElement = document.getElementById("typing-text");
-
-function typeWriter() {
-  if (index < text.length) {
-    textElement.innerHTML += text.charAt(index);
-    index++;
-    setTimeout(typeWriter, speed);
-  }
+function toggleMenu() {
+  const menu = document.querySelector(".links");
+  menu.classList.toggle("active");
 }
-
-window.onload = typeWriter;
